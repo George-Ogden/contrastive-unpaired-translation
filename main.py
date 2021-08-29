2c6f25dcf7ad046cd5fc0767849c439684af0622
George Ogden <George-Ogden@outlook.com>
2021-08-29 10:31:00 +0100

    commented code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 
 from argparse import ArgumentParser
 
+# create a parser with arguments
 parser = ArgumentParser(description="Face Extractor and Portrait Generator")
 
 parser.add_argument("--verbose","-v",action="store_true",help="print extra messages")
@@ -34,32 +35,44 @@ folder.set_defaults(action="folder")
 parser.set_defaults(CUT_mode='CUT', checkpoints_dir='./checkpoints', crop_size=200, direction='AtoB', epoch='latest', flip_equivariance=False, init_gain=0.02, init_type='xavier', input_nc=3, isTrain=False, load_size=256, model='cut', name='painter', nce_idt=True, nce_layers='0,4,8,12,16', netF='mlp_sample', netF_nc=256, netG='resnet_9blocks', ngf=64, no_antialias=False, no_antialias_up=False, no_dropout=True, no_flip=True, normG='instance', output_nc=3, preprocess=[])
 
 args = parser.parse_args()
+if args.verbose:
+    print("Initialising models")
+
+# initialise models
 init(args.gpu_ids[0])
 model = create_model(args)
 model.setup(args)
 model.parallelize()
 model.eval()
+
+# create a transform
 transform = get_transform(args)
 
 def generate(image,opt):
     print(image)
+    # open image
     original = Image.open(image)
     if opt.verbose:
         print("Extracting faces")
+    # extract faces
     faces = list(resize(np.array(original),opt))
     if len(faces) == 0:
         print("No faces detected")
         return cv2.cvtColor(np.array(original),cv2.COLOR_BGR2RGB)
+    # convert faces to tensor
     x = torch.stack([transform(Image.fromarray(face)) for face in faces])
     model.real_A = x
     if opt.verbose:
         print("Generating fake samples")
+    # forward propagate faces
     model.forward()
     if opt.verbose:
         print("Collating results")
+    # concatenate faces
     output = np.concatenate([tensor2im(image.unsqueeze(0)) for image in model.fake])
     faces = np.concatenate([face for face in faces])
     image = np.concatenate((faces,output),axis=1)
+    # add faces to right of image
     original = np.array(original)
     if original.shape[0] > image.shape[0]:
         x = int(original.shape[0] / 2 - image.shape[0] / 2)
@@ -67,19 +80,24 @@ def generate(image,opt):
     else:
         original = cv2.resize(original,(int(original.shape[1] * image.shape[0] / original.shape[0]),image.shape[0]))
     image = np.concatenate((original,image),axis=1)
+    # return correctly coloured image
     return cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
 if __name__ == "__main__":
     if args.action == "image":
+        # convert a single image
         result = generate(args.input_image,args)
         create_folder(args.save_path)
         cv2.imwrite(args.save_path,result)
     elif args.action == "folder":
         folder = args.input_folder
         if not folder.endswith("/") and not folder.endswith("\\"):
+            # make sure folder ends with /
             folder += "/"
         for extension in args.extensions:
+            # loop through files
             for image in glob(f"{folder}**/*.{extension}",recursive=True):
+                # convert each file
                 result = generate(image,args)
                 save_path = os.path.join(args.output_folder,os.path.relpath(image,folder))
                 create_folder(save_path)
--- util/face.py
+++ util/face.py
@@ -13,6 +13,7 @@ from utils.torch_utils import select_device
 model = None
 device = None
 def init(gpu):
+    # intialise model
     global model, device
     device = select_device(str(gpu))
     model = attempt_load("checkpoints/face/yolo.pt", map_location=device)
@@ -23,6 +24,7 @@ def xywh(im0):
     """
     returns x,y,w,h
     """
+    # format image
     stride = int(model.stride.max())
     img = letterbox(im0, 512,stride=stride)[0]
     img = img[:, :, ::-1].transpose(2, 0, 1)
@@ -31,27 +33,34 @@ def xywh(im0):
     img = img.half()
     img /= 255.0
     if img.ndimension() == 3: img = img.unsqueeze(0)
+    # generate prodictions
     pred = model(img, augment=False)[0]
     pred = non_max_suppression(pred, 0.5, 0.45, agnostic=True)
     if len(pred):
         det = pred[0]
+        # resize predictions
         det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
         for *xyxy, conf, cls in reversed(det):
             x1, y1, x2, y2 = xyxy
+            # return each prediction and x,y,w,h
             yield int((x1+x2)/2), int((y1+y2)/2), int(x2-x1), int(y2-y1)
 
 def crop(image,opt):
+    # centre crop image
     x = int(opt.load_size / 2 - opt.crop_size / 2)
     h = opt.crop_size
     return image[x:x+h,x:x+h]
 
 def resize(image,opt):
     shape = [opt.load_size] * 2
+    # values taken from initial dataset
     width = 86
     height = 119
     for x,y,w,h in xywh(image):
         if w < opt.min_face_size:
+            # ignore small faces
             continue
         ratio = np.sqrt(float(width*height)/float(w*h))
+        # resize image using linear algebra
         yield crop(cv2.warpAffine(image,np.float32([[ratio,0,int(shape[0]/2-ratio*x)],[0,ratio,int(shape[1]/2-ratio*y)]]),(shape[0],shape[1])),opt)
         
